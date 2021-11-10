75ac73a6784021ef164edb43f3b5aa31c2ccc7cb
penicillin0 <ayahitonakamura@gmail.com>
2021-11-11 05:54:27 +0900

    rm default content

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
 import "./App.css";
-import logo from "./logo.svg";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>scrapbox-list-marker</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
