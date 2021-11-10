b0947ddb6376ee0c3fe73ba980433b079f815f51
penicillin0 <ayahitonakamura@gmail.com>
2021-11-11 06:49:18 +0900

    css change test

--- js/test.js
+++ js/test.js
@@ -1,1 +1,5 @@
-console.log("test");
+var stylesheet = document.styleSheets[0];
+console.log(stylesheet);
+console.log(stylesheet.cssRules[1384]);
+console.log(stylesheet.cssRules[1384].style.backgroundColor);
+stylesheet.cssRules[1384].style.backgroundColor = "rgb(3, 255, 255)";
