e9229c0a0af2ee096e0c060949ca2f90ae11610f
penicillin0 <ayahitonakamura@gmail.com>
2021-11-11 05:52:46 +0900

    tweak

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import logo from "./logo.svg";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>scrapbox-list-marker</p>
         <a
           className="App-link"
           href="https://reactjs.org"
