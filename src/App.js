09243fbd41ae5e237bee9b7bb4e8150c72dde382
penicillin0 <ayahitonakamura@gmail.com>
2021-11-11 10:37:57 +0900

    initial layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>scrapbox-list-marker</p>
-      </header>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
