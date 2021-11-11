1064aa10ba4237d0024e5fd3917ec08c25819976
penicillin0 <ayahitonakamura@gmail.com>
2021-11-11 11:01:13 +0900

    introduce eslint

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    webextensions: true,
+    es6: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'eslint-config-google',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'prettier',
+  ],
+  plugins: ['react'],
+  parserOptions: {
+    ecmaVersion: 13,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  ignorePatterns: ['/src/icon/*'],
+  rules: {
+    'no-console': 'warn',
+    'require-jsdoc': 'off',
+    'react/jsx-uses-react': 'error',
+    'react/jsx-uses-vars': 'error',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
--- js/test.js
+++ js/test.js
@@ -1,36 +1,38 @@
-var stylesheet = document.styleSheets[0];
+'use strict';
 
-stylesheet.cssRules[1384].style.backgroundColor = "rgb(230, 255, 255)";
+const stylesheet = document.styleSheets[0];
+
+stylesheet.cssRules[1384].style.backgroundColor = 'rgb(230, 255, 255)';
 const indentCSSRule = new Map([
   [
-    "1",
+    '1',
     {
-      before: " .c-",
+      before: ' .c-',
       after:
-        " + .dot {height: .4em !important;width: .4em !important;border-color: black !important;border: solid .1em rgba(0,0,0,0.65) !important;background-color: rgb(255,255,255) !important;}",
+        ' + .dot {height: .4em !important;width: .4em !important;border-color: black !important;border: solid .1em rgba(0,0,0,0.65) !important;background-color: rgb(255,255,255) !important;}',
     },
   ],
   [
-    "2",
+    '2',
     {
-      before: " .c-",
+      before: ' .c-',
       after:
-        " + .dot {height: .4em !important;width: .4em !important;border-radius: 25% !important;background-color: rgba(0,0,0,0.65) !important;}",
+        ' + .dot {height: .4em !important;width: .4em !important;border-radius: 25% !important;background-color: rgba(0,0,0,0.65) !important;}',
     },
   ],
   [
-    "3",
+    '3',
     {
-      before: " .c-",
+      before: ' .c-',
       after:
-        " + .dot {height: .4em !important; width: .4em !important;border-radius: 25% !important; border: solid .1em rgba(0,0,0,0.65) !important;background-color: rgb(255,255,255) !important; }",
+        ' + .dot {height: .4em !important; width: .4em !important;border-radius: 25% !important; border: solid .1em rgba(0,0,0,0.65) !important;background-color: rgb(255,255,255) !important; }',
     },
   ],
 ]);
 
 const insertIndentCSSRule = (indentNum) => {
   const modNum = String(indentNum % 4);
-  if (modNum === "0") return;
+  if (modNum === '0') return;
 
   const before = indentCSSRule.get(modNum).before;
   const after = indentCSSRule.get(modNum).after;
