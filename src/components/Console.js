24f04fb217346092ddb264590c906f0127094add
penicillin0 <ayahitonakamura@gmail.com>
2021-11-14 12:18:15 +0900

    sync to storage

--- src/components/Console.js
+++ src/components/Console.js
@@ -2,42 +2,71 @@ import React, { useEffect, useState } from 'react';
 import Select from 'react-select';
 
 const options = [
-  { value: 'circleBlack', label: '●' },
-  { value: 'circleWhite', label: '○' },
-  { value: 'squareBlack', label: '■' },
-  { value: 'squareWhite', label: '□' },
+  { value: '●', label: '●' },
+  { value: '○', label: '○' },
+  { value: '■', label: '■' },
+  { value: '□', label: '□' },
 ];
 
 const initialState = [
-  { value: '', label: 'indent-1' },
-  { value: '', label: 'indent-2' },
-  { value: '', label: 'indent-3' },
-  { value: '', label: 'indent-4' },
+  { value: '○', label: 'indent-1' },
+  { value: '○', label: 'indent-2' },
+  { value: '○', label: 'indent-3' },
+  { value: '○', label: 'indent-4' },
 ];
 
 export const Console = () => {
   const [indentOptions, setIndentOptions] = useState(initialState);
 
   useEffect(() => {
-    localStorage.getItem('scrapboxIndentOption', (result) => {
-      if (!result.scrapboxIndentOption) {
-        localStorage.setItem('scrapboxIndentOption', indentOptions);
-      }
-      setIndentOptions(result.scrapboxIndentOption || initialState);
-    });
+    const scrapboxIndentOption = localStorage.getItem('scrapboxIndentOption');
+
+    if (!scrapboxIndentOption) {
+      localStorage.setItem(
+        'scrapboxIndentOption',
+        JSON.stringify(initialState)
+      );
+      setIndentOptions(initialState);
+    }
+    setIndentOptions(JSON.parse(scrapboxIndentOption));
   }, []);
+
+  const handleOnChange = (newValue, metaAction) => {
+    const value = newValue.value;
+    const label = metaAction.name;
+
+    const newIndentOptions = indentOptions.map((option) => {
+      if (option.label === label) {
+        return { value, label };
+      }
+      return option;
+    });
+
+    setIndentOptions(newIndentOptions);
+    localStorage.setItem(
+      'scrapboxIndentOption',
+      JSON.stringify(newIndentOptions)
+    );
+  };
 
   return (
     <>
-      {indentOptions.map((indentOption) => {
-        return (
-          <Select
-            key={indentOption.label}
-            name={indentOption.label}
-            options={options}
-          ></Select>
-        );
-      })}
+      {indentOptions
+        ? indentOptions.map((indentOption) => {
+            return (
+              <Select
+                value={{
+                  value: indentOption.value,
+                  label: indentOption.value,
+                }}
+                onChange={handleOnChange}
+                key={indentOption.label}
+                name={indentOption.label}
+                options={options}
+              ></Select>
+            );
+          })
+        : null}
     </>
   );
 };
