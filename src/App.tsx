ae3c4659e8309cc5c5246a3e2375f527429f2dd1
penicillin0 <ayahitonakamura@gmail.com>
2021-11-14 18:01:01 +0900

    とりあえずbuildできるとこまで

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,12 @@ import React from 'react';
 import './App.css';
 import { Console } from './components/Console';
 
-function App() {
-  // const [indentOptions, setIndentOptions] = useState(initialState);
-
+const App: React.FC = () => {
   return (
     <div className="App">
       <Console />
-      {/* <Console indentOption={indentOption} /> */}
     </div>
   );
-}
+};
 
 export default App;
