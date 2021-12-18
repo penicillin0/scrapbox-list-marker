f58304f7e4ef1dfab3405511da7cc26b03c85991
penicillin0 <ayahitonakamura@gmail.com>
2021-12-19 00:15:56 +0900

    wip

---
+++ contentScripts/cssRuleGenerate.js
@@ -0,0 +1,3 @@
+export const consoleTest = () => {
+  console.log('++++++++++test++++++++++');
+};
--- contentScripts/scrapboxCssScript.js
+++ contentScripts/scrapboxCssScript.js
@@ -1,142 +1,149 @@
 'use strict';
 
-const indentCSSRule = new Map([
-  [
-    '●',
-    {
-      before: ' .c-',
-      after:
-        ' + .dot {height: .4em !important;width: .4em !important;border-color: rgba(0,0,0,0.65) !important;border: solid .1em rgba(0,0,0,0.65) !important;background-color: rgba(0,0,0,0.65) !important;}',
-    },
-  ],
-  [
-    '○',
-    {
-      before: ' .c-',
-      after:
-        ' + .dot {height: .4em !important;width: .4em !important;border-color: black !important;border: solid .1em rgba(0,0,0,0.65) !important;background-color: rgb(255,255,255) !important;}',
-    },
-  ],
-  [
-    '■',
-    {
-      before: ' .c-',
-      after:
-        ' + .dot {height: .4em !important;width: .4em !important;border-radius: 25% !important;background-color: rgba(0,0,0,0.65) !important;}',
-    },
-  ],
-  [
-    '□',
-    {
-      before: ' .c-',
-      after:
-        ' + .dot {height: .4em !important; width: .4em !important;border-radius: 25% !important; border: solid .1em rgba(0,0,0,0.65) !important;background-color: rgb(255,255,255) !important; }',
-    },
-  ],
-]);
+import { consoleTest } from './cssRuleGenerate.js';
 
-const insertIndentCSSRule = (scrapboxIndentOptions) => {
-  // delete existing rules
-  const cssRules = document.styleSheets[0].cssRules;
-  const cssRulesNum = cssRules.length;
-  for (let i = cssRulesNum - 1; i >= 0; i--) {
-    const cssRule = cssRules[i];
-    const cssSelector = cssRule.selectorText;
+export function main() {
+  const indentCSSRule = new Map([
+    [
+      '●',
+      {
+        before: ' .c-',
+        after:
+          ' + .dot {height: .4em !important;width: .4em !important;border-color: rgba(0,0,0,0.65) !important;border: solid .1em rgba(0,0,0,0.65) !important;background-color: rgba(0,0,0,0.65) !important;}',
+      },
+    ],
+    [
+      '○',
+      {
+        before: ' .c-',
+        after:
+          ' + .dot {height: .4em !important;width: .4em !important;border-color: black !important;border: solid .1em rgba(0,0,0,0.65) !important;background-color: rgb(255,255,255) !important;}',
+      },
+    ],
+    [
+      '■',
+      {
+        before: ' .c-',
+        after:
+          ' + .dot {height: .4em !important;width: .4em !important;border-radius: 25% !important;background-color: rgba(0,0,0,0.65) !important;}',
+      },
+    ],
+    [
+      '□',
+      {
+        before: ' .c-',
+        after:
+          ' + .dot {height: .4em !important; width: .4em !important;border-radius: 25% !important; border: solid .1em rgba(0,0,0,0.65) !important;background-color: rgb(255,255,255) !important; }',
+      },
+    ],
+  ]);
 
-    if (cssSelector === undefined) continue;
+  const insertIndentCSSRule = (scrapboxIndentOptions) => {
+    // delete existing rules
+    const cssRules = document.styleSheets[0].cssRules;
+    const cssRulesNum = cssRules.length;
+    for (let i = cssRulesNum - 1; i >= 0; i--) {
+      const cssRule = cssRules[i];
+      const cssSelector = cssRule.selectorText;
 
-    if (cssSelector.match(/^(\.(c-\d+) \+ \.dot)$/)) {
-      document.styleSheets[0].deleteRule(i);
-    }
-  }
+      if (cssSelector === undefined) continue;
 
-  // insert new rules
-  scrapboxIndentOptions.map((scrapboxIndentOption) => {
-    const indentNum = Number(scrapboxIndentOption.label.replace(/[^0-9]/g, ''));
-
-    for (let i = 0; i < 20; i++) {
-      if (i % 4 === indentNum - 1) {
-        const before = indentCSSRule.get(scrapboxIndentOption.value).before;
-        const after = indentCSSRule.get(scrapboxIndentOption.value).after;
-        document.styleSheets[0].insertRule(before + String(i) + after);
+      if (cssSelector.match(/^(\.(c-\d+) \+ \.dot)$/)) {
+        document.styleSheets[0].deleteRule(i);
       }
     }
-  });
-};
 
-const indentLineCSS = [
-  `.app:not(.presentation) .indent-mark .char-index:not(:nth-last-child(1)):not(:nth-last-child(2)) { position: relative; }`,
-  `.app:not(.presentation) .indent-mark .char-index:not(:nth-last-child(1)):not(:nth-last-child(2))::before {
+    // insert new rules
+    scrapboxIndentOptions.map((scrapboxIndentOption) => {
+      const indentNum = Number(
+        scrapboxIndentOption.label.replace(/[^0-9]/g, '')
+      );
+
+      for (let i = 0; i < 20; i++) {
+        if (i % 4 === indentNum - 1) {
+          const before = indentCSSRule.get(scrapboxIndentOption.value).before;
+          const after = indentCSSRule.get(scrapboxIndentOption.value).after;
+          document.styleSheets[0].insertRule(before + String(i) + after);
+        }
+      }
+    });
+  };
+
+  const indentLineCSS = [
+    `.app:not(.presentation) .indent-mark .char-index:not(:nth-last-child(1)):not(:nth-last-child(2)) { position: relative; }`,
+    `.app:not(.presentation) .indent-mark .char-index:not(:nth-last-child(1)):not(:nth-last-child(2))::before {
       content: " ";
       position: absolute;
       left: 43%;
       margin: -4px 0;
       border-left: 2px solid #dcdcdc;
     }`,
-];
+  ];
 
-const insertIndentLineCSSRule = (isLining, indentColor) => {
-  // delete existing rules
-  const cssRules = document.styleSheets[0].cssRules;
-  const cssRulesNum = cssRules.length;
-  for (let i = cssRulesNum - 1; i >= 0; i--) {
-    const cssRule = cssRules[i];
-    const cssSelector = cssRule.selectorText;
+  const insertIndentLineCSSRule = (isLining, indentColor) => {
+    // delete existing rules
+    const cssRules = document.styleSheets[0].cssRules;
+    const cssRulesNum = cssRules.length;
+    for (let i = cssRulesNum - 1; i >= 0; i--) {
+      const cssRule = cssRules[i];
+      const cssSelector = cssRule.selectorText;
 
-    if (cssSelector === undefined) continue;
+      if (cssSelector === undefined) continue;
 
-    if (
-      cssSelector.match(
-        /^\.app:not\(\.presentation\) .indent-mark .char-index:not\(:nth-last-child\(1\)\):not\(:nth-last-child\(2\)\)*/
-      )
-    ) {
-      document.styleSheets[0].deleteRule(i);
-    }
-  }
-
-  // insert new rules
-  if (isLining) {
-    indentLineCSS.map((css) => {
-      if (css.includes('#dcdcdc')) {
-        css = css.replace('#dcdcdc', indentColor);
+      if (
+        cssSelector.match(
+          /^\.app:not\(\.presentation\) .indent-mark .char-index:not\(:nth-last-child\(1\)\):not\(:nth-last-child\(2\)\)*/
+        )
+      ) {
+        document.styleSheets[0].deleteRule(i);
       }
-      document.styleSheets[0].insertRule(css);
+    }
+
+    // insert new rules
+    if (isLining) {
+      indentLineCSS.map((css) => {
+        if (css.includes('#dcdcdc')) {
+          css = css.replace('#dcdcdc', indentColor);
+        }
+        document.styleSheets[0].insertRule(css);
+      });
+    }
+  };
+
+  const makerAttachment = () => {
+    chrome.storage.local.get('scrapboxIndentOption', (result) => {
+      const scrapboxIndentOptions = result.scrapboxIndentOption;
+      insertIndentCSSRule(scrapboxIndentOptions);
     });
-  }
-};
+  };
 
-const makerAttachment = () => {
-  chrome.storage.local.get('scrapboxIndentOption', (result) => {
-    const scrapboxIndentOptions = result.scrapboxIndentOption;
-    insertIndentCSSRule(scrapboxIndentOptions);
-  });
-};
+  const liningAttachment = () => {
+    chrome.storage.local.get('scrapboxIndentLining', (result) => {
+      const isLining = result.scrapboxIndentLining;
 
-const liningAttachment = () => {
-  chrome.storage.local.get('scrapboxIndentLining', (result) => {
-    const isLining = result.scrapboxIndentLining;
-
-    chrome.storage.local.get('scrapboxIndentLineColor', (result) => {
-      const scrapboxIndentLineColor =
-        result.scrapboxIndentLineColor || '#dcdcdc';
-      insertIndentLineCSSRule(isLining, scrapboxIndentLineColor);
+      chrome.storage.local.get('scrapboxIndentLineColor', (result) => {
+        consoleTest();
+        const scrapboxIndentLineColor =
+          result.scrapboxIndentLineColor || '#dcdcdc';
+        insertIndentLineCSSRule(isLining, scrapboxIndentLineColor);
+      });
     });
+  };
+
+  // update
+  chrome.runtime.onMessage.addListener((request) => {
+    if (request === 'scrapbox_list_maker') {
+      makerAttachment();
+    }
+    if (
+      request === 'scrapbox_indent_lining' ||
+      request === 'scrapbox_indent_lining_color'
+    ) {
+      liningAttachment();
+    }
   });
-};
 
-// update
-chrome.runtime.onMessage.addListener((request) => {
-  if (request === 'scrapbox_list_maker') {
-    makerAttachment();
-  }
-  if (
-    request === 'scrapbox_indent_lining' ||
-    request === 'scrapbox_indent_lining_color'
-  ) {
-    liningAttachment();
-  }
-});
-
-// initialize
-makerAttachment();
-liningAttachment();
+  // initialize
+  makerAttachment();
+  liningAttachment();
+}
